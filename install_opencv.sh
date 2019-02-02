9f9aaf38243e5995e999caf72e609309d6a9c1fe
Igor <igor.ryadinskii@gmail.com>
2019-02-02 22:52:11 +0200

    remove vlc

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-sudo apt install build-essential cmake git pkg-config libgtk-3-dev
-sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
-sudo apt install libjpeg-dev libpng-dev libtiff-dev gfortran openexr
-sudo apt install python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev
+#sudo apt install build-essential cmake git pkg-config libgtk-3-dev
+#sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
+#sudo apt install libjpeg-dev libpng-dev libtiff-dev gfortran openexr
+#sudo apt install python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev
+
+mkdir ~/opencv_build && cd ~/opencv_build
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+
 cd ~/opencv_build/opencv
 mkdir build && cd build
 cmake -DWITH_LIBV4L=ON ..
